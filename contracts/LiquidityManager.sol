2129240d1dec5ca3ea9591349427df49a8bbe267
AaronZaki <AaronZaki88@outlook.com>
2025-03-06 02:47:05 +0800

    Update LiquidityManager.sol

--- contracts/LiquidityManager.sol
+++ contracts/LiquidityManager.sol
@@ -12,22 +12,22 @@ contract LiquidityManager is AccessControl {
     event LiquidityRemoved(address indexed user, uint256 amount);
     
     constructor(address tokenAddress) {
-        require(tokenAddress != address(0), "Invalid token address");
+        require(tokenAddress != address(0), "Invalid token address.");
         token = ERC20Token(tokenAddress);
     }
     
     function addLiquidity(uint256 amount) external {
         require(amount > 0, "Amount must be greater than zero");
-        require(token.transferFrom(msg.sender, address(this), amount), "Transfer failed");
+        require(token.transferFrom(msg.sender, address(this), amount), "Transfer failed.");
         liquidity[msg.sender] += amount;
         emit LiquidityAdded(msg.sender, amount);
     }
     
     function removeLiquidity(uint256 amount) external {
-        require(amount > 0, "Amount must be greater than zero");
+        require(amount > 0, "Amount must be greater than zero.");
         require(liquidity[msg.sender] >= amount, "Insufficient liquidity");
         liquidity[msg.sender] -= amount;
-        require(token.transfer(msg.sender, amount), "Transfer failed");
+        require(token.transfer(msg.sender, amount), "Transfer failed.");
         emit LiquidityRemoved(msg.sender, amount);
     }
 }
